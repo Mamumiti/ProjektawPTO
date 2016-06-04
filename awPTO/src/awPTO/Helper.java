55faf30f1535c6b34faca125da5cbdc3b99f56d5
zajac <zajac@Lenovo>
2016-06-05 00:47:04 +0200

    commit 3

--- awPTO/src/awPTO/Helper.java
+++ awPTO/src/awPTO/Helper.java
@@ -7,38 +7,59 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Helper {
-	
-	private String 	line;
+
+	private String line;
+	private Set<String> variables;
+	private Set<String> binaryMatrix;
 
 	public Helper(String filePath) {
-		try (
-			    InputStream fis = new FileInputStream(filePath);
-			    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-			    BufferedReader br = new BufferedReader(isr);
-			) {
+		try (InputStream fis = new FileInputStream(filePath);
+				InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+				BufferedReader br = new BufferedReader(isr);) {
 			line = br.readLine();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		variables = getVariablesTree();
+		binaryMatrix = generateBinaryMatrix();
 	}
 
 	public String getLine() {
 		return line;
 	}
-	
+
 	public int getVariableQuantity() {
-		int temp = 0;
-		for (int i = 0; i < line.length(); i++) {
-			if(Character.isLetter(line.charAt(i))) {
-				temp++;
-			}
-		}	
-		return temp;
+		return variables.size();
 	}
-	
-	
+
+	private Set<String> getVariablesTree() {
+		Set<String> tree = new TreeSet<>();
+		for (char c : line.toCharArray()) {
+			if (Character.isLetter(c)) {
+				tree.add(Character.toString(c));
+			}
+		}
+		return tree;
+	}
+
+	public Set<String> generateBinaryMatrix() {
+		Set<String> tree = new TreeSet<>();
+		for (int i = 0; i < Math.pow(2, getVariableQuantity()); i++) {
+			StringBuilder st = new StringBuilder();
+			for (int j = 0; j < getVariableQuantity() - Integer.toBinaryString(i).length(); j++) {
+
+				st.append(0);
+			}
+			st.append(Integer.toBinaryString(i));
+			tree.add(st.toString());
+		}
+		return tree;
+	}
 
 }
--- awPTO/src/awPTO/awPTO.java
+++ awPTO/src/awPTO/awPTO.java
@@ -3,30 +3,19 @@ package awPTO;
 public class awPTO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		Helper helper = new Helper("input.txt");
 		
-		//String input = "(A|B)&C~&D&~E";
+
 		String input = helper.getLine();
 		System.out.println(helper.getVariableQuantity());
 	      String output;
 	      InToPost theTrans = new InToPost(input);
 	      output = theTrans.doTrans(); 
 	      System.out.println("input is:"+input+" \nPostfix is " + output + '\n');
-	      
-	      for (int i = 0; i <= helper.getVariableQuantity(); i++) {
-			System.out.println(Integer.toBinaryString(i));
-		}
-	      //System.out.println(theTrans.getTheStack().pop());
-	      
-//	      for (int i = 0; i<=1;i++) {
-//	    	  for (int j = 0; j <=1; j++) {
-//	    		  for (int j2 = 0; j2 <=1; j2++) {
-//					System.out.print(i+" "+j+" "+j2+"\n");
-//				}
-//			}
-//	      }
-		
+	 
+	      helper.generateBinaryMatrix();
+
 	}
 
 }
